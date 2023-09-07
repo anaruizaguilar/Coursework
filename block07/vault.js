8303005b24c1da3eb52527f129871fcec61b9af6
anaruizaguilar <anaruiza@SEA-1800685161.ant.amazon.com>
2023-09-07 07:27:18 -0500

    First commit of Coursework folder

---
+++ block07/vault.js
@@ -0,0 +1,10 @@
+//Pseudocode: String alerting the user of the combination.
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//Arithmetic function for variable A.
+const comboA = 5 * 2;
+//Arithmetic function for variable B.
+const comboB = 120 / 3;
+//Arithmetic function for variable C.
+const comboC = 25 + 14;
+//Dialog box showing the message + combination.
+console.log(message + comboA + comboB + comboC);
